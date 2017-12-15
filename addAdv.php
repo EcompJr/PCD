f0c2492c6588014e2eaf43ef4be59a56c625fdc2
Pedro Henrique Gomes dos Santos <peuh1997@gmail.com>
2017-12-15 14:47:05 -0200

    Add files via upload

---
+++ addAdv.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PCD - ECOMP Jr.</title>
+    <meta charset="UTF-8">    
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+        crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <script src="assets/js/jquery-3.2.1.js"></script>
+    <script src="assets/js/javascript.js"></script>
+
+    <!-- Icon -->
+    <link rel="icon" href="assets/images/ecomp/logo.png">    
+</head>
+
+<body>
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="#">
+                <img src="assets/images/ecomp/logo.png" width="30">
+                </a>
+                <!-- <a class="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>-->
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll scrollSuave" href="#about">Sobre</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll scrollSuave" href="#rules">Regras</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll scrollSuave" href="pcd.html">Membros</a>
+                    </li>
+
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1 id="message"></h1>
+                <hr>
+                <p id="txtheader">Programa de Controle Disciplinar, também conhecido como PCD, aplicado na Empresa Júnior de Engenharia de Computação.</p>
+                <a href="#about" class="btn btn-primary btn-xl page-scroll scrollSuave">Descubra</a>
+
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-blue" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    
+
+                    
+                    <br>
+                    <hr class="light">
+                    <br>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+    
+</body>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+    crossorigin="anonymous"></script>
+
+</html>
---
+++ painel.js
@@ -0,0 +1,103 @@
+function updatePoints() {
+    var x = document.getElementById("#qtdDias").value;
+    document.getElementById("#points").innerHTML = x;
+}
+
+$(document).ready(function(){
+
+	
+	$('select').material_select();
+
+	$('.datepicker').pickadate({
+	    selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15, // Creates a dropdown of 15 years to control year
+		format: 'yyyy-mm-dd', //Formata a data
+ 	 });
+
+	$("#qtdDias").attr('disabled', 'disabled');
+	
+	$("#motivo").change(function() {
+		
+		$("#qtdDias").attr('disabled', 'disabled');
+		
+ 		if ($("#motivo option:selected").val() == "" ){
+
+			$("#points").val("");
+		}
+ 		else if ($("#motivo option:selected").val() == "motivo1" ){
+
+			$("#points").val("4");
+
+		}else if ($("#motivo option:selected").val() == "motivo2" ){
+
+			$("#points").val("2");
+
+		}else if ($("#motivo option:selected").val() == "motivo3" ){
+			
+			//ESCONDER O INPUT QTD DIAS QUANDO ESCOLHER OUTRA OPCAO
+			
+			// if (!($("#qtdDias").is(":visible")) ){
+			// 	$("#motivo").append('<div class="row"><div class="input-field col s8"><input id="qtdDias" type="number" name="qtdDias" placeholder="Qtd dias" min="1" size="2"></div></div>');
+			// }else{
+			// 	$("#qtdDias").change(function() { 	
+			// 		$("#points").val($("#qtdDias").val()*2);
+			// 	});
+			// }
+			
+			$("#qtdDias").removeAttr('disabled');
+			$("#qtdDias").change(function() { 	
+				$("#points").val($("#qtdDias").val()*2);
+			})
+
+
+
+		}else if ($("#motivo option:selected").val() == "motivo4" ){
+
+			$("#points").val("2");
+
+		}else if ($("#motivo option:selected").val() == "motivo5" ){
+
+			$("#points").val("4");
+
+		}else if ($("#motivo option:selected").val() == "motivo6" ){
+				
+			$("#points").val("10");
+		}
+	});
+  		
+	$("#envAdv").click(function(){
+		if($("#motivo option:selected").val()==""){
+			alert("Escolha uma opção!");
+		}
+	});
+	
+	$("#logout").click(function(){
+
+		$.ajax({
+            url: 'http://localhost:8080/routes/routes.php',
+            type: 'get',
+            data:{action:'logoff'},
+            success: function(){
+            alert("Deslogado com sucesso!");
+                location.reload();
+            }
+        });
+	 });
+
+	 $("#regras").click(function(){
+
+        window.location= "http://localhost:8080/view/regras.php";
+    });
+
+    $("#advBTN").click(function(){
+
+      window.location= "http://localhost:8080/view/advertencias.php";
+  });
+
+  $("#contasBTN").click(function(){
+
+      window.location= "http://localhost:8080/view/gerenciarContas.php";
+  });
+	 
+});
+
