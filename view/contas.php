0db1fa19ecf4d384a6e9040b30ebda3685c14bbc
Gustavo Boanerges <g.boanerges2@gmail.com>
2018-02-06 08:21:26 -0300

    Add page contas

---
+++ view/contas.php
@@ -0,0 +1,131 @@
+<?php
+    session_start();
+    require_once("../controller/MembrosController.class.php");
+    if (!isset($_SESSION['auth'])) {
+	//	header("location:../view/login.php?valid=false");	
+		echo "not logged";			
+	}
+    $adm = null;
+    //Verica o cargo do usuario, para exibir botao de gerenciar na navbar
+    //caso este seja administrador
+    if(isset($_SESSION['cargo'])){
+        if($_SESSION['cargo']=="Diretor" || $_SESSION['cargo']=="Conselheiro"){
+            $adm = true;
+        }else{
+            $adm = false;   
+            echo $_SESSION['cargo'];
+        }
+    }
+    $membrosController = new MembrosController();
+	$contas = $membrosController->getContas();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contas PCD</title>
+
+	<script type="text/javascript" src="../assets/js/jquery-3.2.1.js"></script>
+	<!-- <script src="../assets/js/javascript.js"></script> -->
+	<script type="text/javascript" src="../assets/js/painel.js"></script>
+	<script type="text/javascript" src="../assets/js/bootstrap-datepicker.min.js"></script>
+	<script type="text/javascript"src="../assets/js/bootstrap-datepicker.pt-BR.min.js"></script>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/style.css">
+
+
+	<link href="../assets/css/bootstrap-datepicker.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	
+	
+    <!-- Icon -->
+	<link rel="icon" href="../assets/images/ecomp/logo.png"> 
+
+</head>
+<body>
+	<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+				</button>
+				<a class="navbar-brand" href="#">
+				<img src="../assets/images/ecomp/logo.png" width="30">
+				</a>
+				<!-- <a class="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>-->
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<a class="page-scroll scrollSuave" href="#about">Sobre</a>
+					</li>
+					<li>
+						<a class="page-scroll scrollSuave" href="#rules">Regras</a>
+					</li>
+					<li>
+						<a class="page-scroll scrollSuave" href="pcd.php">Membros</a>
+					</li>
+
+
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+	<!-- /.container-fluid -->
+	</nav>
+	<div class="container-fluid">
+	<div class="row">
+		<div id="divConteudo" class="container col-md-offset-2 col-md-8">
+            <div class="row">
+                <div class="col-md-3">
+                    <h4 class="text-justify">Gerenciar Contas</h4>
+                </div>
+                <div class="col-md-offset-7 col-md-2">
+                    <button class="btn btn-primary" onclick="location.href='cadastro.php';">Cadastrar</button>
+                </div>
+            </div>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-12">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Login</th>
+                                    <th>Nome</th>
+                                    <th>Cargo</th>
+                                    <th id="emptyTR">&nbsp;</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <?php 
+                                        for ($i=0; $i < sizeof($contas) ; $i++) {
+                                            $id = $contas[$i]['id'];
+                                            echo "<tr>";
+                                            echo "<td>".$contas[$i]['login']."</td>";
+                                            echo "<td>".$contas[$i]['name']."</td>";
+                                            echo "<td>".$contas[$i]['occupation']."</td>";
+                                            
+                                            echo "<td><button id='btnEdit' type=\"button\"  class=\"botaoE btn-primary\" onclick=\"editId($id)\"><span class=\"glyphicon glyphicon-edit\" aria-hidden=\"true\"></span></button>&nbsp&nbsp<button id='btnDelete' type=\"button\"  class=\"botaoD btn-danger\" onclick=\"deleteId($id)\"><span class=\"glyphicon glyphicon-trash\" aria-hidden=\"true\"></span></button></td>";
+                                            echo "</tr>";	
+                                            				
+                                        }
+                                    ?>
+                                </tr>
+                            </tbody>
+                        </table>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+</div>
+</body>
+</html>
+
