6d1ab4c508434d83b825d76e781d0655133a71bc
Gustavo Boanerges <g.boanerges2@gmail.com>
2018-02-20 08:29:40 -0300

    Add logout function

--- assets/js/painel.js
+++ assets/js/painel.js
@@ -65,18 +65,19 @@ $(document).ready(function(){
 		}
 	});
   		
+	//Logout
 	$("#logout").click(function(){
-
+			
 		$.ajax({
-            url: 'http://localhost:8081/routes/routes.php',
-            type: 'get',
-            data:{action:'logoff'},
-            success: function(){
-            alert("Deslogado com sucesso!");
-                location.reload();
-            }
-        });
-	 });
+			url: 'http://localhost:8081/routes/routes.php',
+			type: 'get',
+			data:{action:'logoff'},
+			success: function(){
+				alert("Deslogado com sucesso!");
+				window.location= "http://localhost:8081/view/login.php?logout=true";
+			}
+		});
+	});
 
 // 	 $("#regras").click(function(){
 
