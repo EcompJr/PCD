9955dd12fb9a2d2687dd96c396f18e08b6fa9c44
Gustavo Boanerges <g.boanerges2@gmail.com>
2018-02-20 07:58:49 -0300

    Add navbarAdm file, add require once on adm pages

--- view/advertencias.php
+++ view/advertencias.php
@@ -43,39 +43,9 @@
 
 </head>
 <body>
-	<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
-		<div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-				</button>
-				<a class="navbar-brand" href="#">
-				<img src="../assets/images/ecomp/logo.png" width="30">
-				</a>
-				<!-- <a class="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>-->
-			</div>
-
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav navbar-right">
-					<li>
-						<a class="page-scroll scrollSuave" href="#about">Sobre</a>
-					</li>
-					<li>
-						<a class="page-scroll scrollSuave" href="#rules">Regras</a>
-					</li>
-					<li>
-						<a class="page-scroll scrollSuave" href="pcd.php">Membros</a>
-					</li>
-
-
-				</ul>
-			</div>
-			<!-- /.navbar-collapse -->
-		</div>
-	<!-- /.container-fluid -->
-	</nav>
+    <?php
+        require_once 'navbarAdm.php';
+    ?>
 	<div class="container-fluid">
 	<div class="row">
 		<div id="divConteudo" class="container col-md-offset-2 col-md-8">
