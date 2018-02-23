a5f6a5d60b8baa60bd2a8424ca53f9ec4cd2e11f
PHGSantos <peuh_fsa@hotmail.com>
2018-02-23 12:43:50 -0300

    fixed link at index

--- index.php
+++ index.php
@@ -197,7 +197,7 @@
                         <br>
                         <hr class="dark">
                         <br>
-                        <a href="view/pcd.php" class="page-scroll btn btn-primary btn-xl sr-button scrollSuave">Conheça os Membros</a>
+                        <a href="Views/pcd.php" class="page-scroll btn btn-primary btn-xl sr-button scrollSuave">Conheça os Membros</a>
                     </div>
                 </div>
             </div>
