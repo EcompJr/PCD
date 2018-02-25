5e806116f9d1c5a9667dddb11971033858c89062
Gustavo Boanerges <g.boanerges2@gmail.com>
2018-02-25 12:45:58 -0300

    Fix js endless error

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
                     clearInterval(interval2);
                 appear2 = true;
             }, 100);
-    }());
+    });
 
     //$("#logo").hide();
 
